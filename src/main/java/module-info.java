33c90d238d25a5cd1b654e68f5700f38211882f6
DamaiSilalahi <damailalahi0406@gmail.com>
2025-06-16 03:52:53 +0700

    first

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.uaspbo_habittracker_27bulanmei {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.uaspbo_habittracker_27bulanmei to javafx.fxml;
+    exports com.example.uaspbo_habittracker_27bulanmei;
+}
